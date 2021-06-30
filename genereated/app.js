940c429094fa7f377bb585817b806e5970246f83
Sagar Ghimire <sagar.ghimire@kotuko.it>
2021-06-30 11:16:56 +0545

    added ui

--- genereated/app.js
+++ genereated/app.js
@@ -2,6 +2,7 @@
 import express from 'express'
 import morgan from 'morgan'
 import cors from 'cors'
+import * as path from 'path'
 // import { errorHandler } from './middleware/error';
 import userRouter from './api/user/router.js'
 // import taskRouter from './api/task/router.js'
@@ -9,14 +10,19 @@ import userRouter from './api/user/router.js'
 
 const app = express()
 
-
 app.use(cors())
 app.use(morgan('dev'));
 app.use(express.json());
 
+app.use(express.static(path.join(path.resolve(), 'public')))
+
 app.use('/users', userRouter)
 // app.use('/tasks', taskRouter)
 
+
+app.get('/', function (req, res) {
+    res.sendFile('index.html');
+});
 // error handler
 // app.use(errorHandler)
 
